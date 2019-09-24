5ec89afc3790f7e8ac222db7f2085c7dd39238d5
Franco Nieva <franconieva86@gmail.com>
2019-09-24 16:09:47 -0300

    Snapshots testing exceptions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,23 @@ describe('Snapshot testing', () => {
  test('Snapshot', () => {
   expect(getCharacter(rick)).toMatchSnapshot();
  });
+
+ test('Snapshot will always fail', () => {
+  const user = {
+    createdAt: new Date(),
+    id: Math.floor(Math.random() * 20),
+  }
+  expect(user).toMatchSnapshot();
+ });
+
+ test('Snapshot with exception', () => {
+  const user = {
+    name: "Franco Nieva",
+    id: Math.floor(Math.random() * 20),
+  }
+  expect(user).toMatchSnapshot({
+    id: expect.any(Number)
+  });
+ });
+
 });
